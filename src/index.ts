9555ed48dbf8343368b863bd1011c3cc1b16124e
Alex Gerasimov <alcordom@gmail.com>
2024-07-29 23:42:15 +0400

    added nodemon settings and ts-node package

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
-dotenv.config();
-
-const PORT = process.env.PORT || 5000;
-const app = express();
+const PORT = process.env.PORT || 5000
+const app = express()
 
 const start = async () => {
-  try {
-    app.listen(PORT, () => console.log('alive'));
-  } catch (error) {
-    console.log(error);
-  }
-};
+	try {
+		app.listen(PORT, () => console.log('alive'))
+	} catch (error) {
+		console.log(error)
+	}
+}
 
-start();
+start()
