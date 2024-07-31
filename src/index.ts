9e7dba85accd2e19f5a2992fe7ebba1fa6cd7a79
Alex Gerasimov <alcordom@gmail.com>
2024-07-31 23:02:03 +0400

    added mongoose settings and user and token models

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import dotenv from 'dotenv'
-dotenv.config()
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
+import mongoose from 'mongoose'
 
-const PORT = process.env.PORT || 5000
+dotenv.config()
+
+const PORT = process.env.PORT || 5001
 const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
 
 const start = async () => {
 	try {
+		await mongoose.connect(process.env.DB_URL!)
 		app.listen(PORT, () => console.log('alive'))
 	} catch (error) {
 		console.log(error)
